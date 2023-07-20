0e5c6f7ad5277794e1068510d85d4ed04f07fe36
Simon Schmid <simon.schmid@braintec.com>
2023-07-20 12:42:44 +0200

    add test module

---
+++ test_module/__manifest__.py
@@ -0,0 +1,17 @@
+##############################################################################
+# Copyright (c) 2021 brain-tec AG (https://bt-group.com)
+# All Right Reserved
+#
+# See LICENSE file for full licensing details.
+##############################################################################
+
+{
+    'name': 'TEST',
+    'version': '12.0.1.0.1',
+    'category': '',
+    'license': 'LGPL',
+    'summary': 'TEST',
+    'author': 'brain-tec AG',
+    'website': 'http://www.braintec-group.com',
+    'installable': True,
+}
